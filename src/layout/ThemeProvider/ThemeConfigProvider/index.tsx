73ac0519dd3b0b4296bf66bf011c8bea365d6105
AlirezaGhadyani <alirezaghadyani16@gmail.com>
2024-02-25 21:50:14 +0330

    feat: add fonts

--- src/layout/ThemeProvider/ThemeConfigProvider/index.tsx
+++ src/layout/ThemeProvider/ThemeConfigProvider/index.tsx
@@ -1,9 +1,11 @@
 "use client";
-import { ThemeProvider } from "@mui/material";
-import themeConfig from "./themeConfig";
-import { useTheme } from "@/hooks";
 import { useMemo } from "react";
+import { ThemeProvider } from "@mui/material";
+import { useTheme } from "@/hooks";
+import { useGlobalContext } from "../../globalContext";
 import { createTheme } from "@mui/material/styles";
+import themeConfig from "./themeConfig";
+import { roboto, vazirmatn } from "./loadFonts";
 import lightPalette from "./lightPalette";
 import darkPalette from "./darkPalette";
 
@@ -11,16 +13,23 @@ export default function ThemeConfigProvider(props: ChildrenComponent) {
   const { children } = props;
 
   const { theme } = useTheme();
+  const { locale } = useGlobalContext();
 
   const muiThemeConfig = useMemo(() => {
     return createTheme({
       ...themeConfig,
+      typography: {
+        ...themeConfig.typography,
+        ...(locale === "fa"
+          ? { fontFamily: vazirmatn.style.fontFamily }
+          : { fontFamily: roboto.style.fontFamily }),
+      },
       palette: {
         mode: theme,
         ...(theme === "light" ? lightPalette : darkPalette),
       },
     });
-  }, [theme]);
+  }, [theme, locale]);
 
   return <ThemeProvider theme={muiThemeConfig}>{children}</ThemeProvider>;
 }
---
+++ src/layout/ThemeProvider/ThemeConfigProvider/loadFonts.ts
@@ -0,0 +1,51 @@
+import localFont from "next/font/local";
+
+export const roboto = localFont({
+  src: [
+    {
+      path: "../../../assets/fonts/roboto/roboto-light.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../../assets/fonts/roboto/roboto-medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../../assets/fonts/roboto/roboto-bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../../assets/fonts/roboto/roboto-black.woff2",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+});
+
+export const vazirmatn = localFont({
+  src: [
+    {
+      path: "../../../assets/fonts/vazirmatn/vazirmatn-light.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../../../assets/fonts/vazirmatn/vazirmatn-medium.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../../../assets/fonts/vazirmatn/vazirmatn-bold.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../../../assets/fonts/vazirmatn/vazirmatn-black.woff2",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+});
